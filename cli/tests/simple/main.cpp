b55275da5c94f4d138e7869067e056a3975753f6
Niran Pon <npon@beyondmpd.com>
2026-03-24 13:37:05 -0600

    Add more test cases for arrays and oneof

--- cli/tests/simple/main.cpp
+++ cli/tests/simple/main.cpp
@@ -6,6 +6,8 @@
 #include <ao/schema/DiskCodec.h>
 #include <ao/schema/NetCodec.h>
 
+#include <ao/utils/Overloaded.h>
+
 #include "simple/AoslCppSimple_messages.h"
 
 #include "../CppTestHelpers.h"
@@ -108,43 +110,70 @@ TEMPLATE_LIST_TEST_CASE("Simple message 5 round trip 1",
 TEMPLATE_LIST_TEST_CASE("Simple message 6 round trip 1",
                         "[simple]",
                         StreamTypes) {
-    messages::TestMessage6 input{
-        .value1 = {1, 2, 3, 4, 5},
+    std::vector<std::vector<int64_t>> testCases{
+        std::vector<int64_t>{},
+        std::vector<int64_t>{1},
+        std::vector<int64_t>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
     };
-    messages::TestMessage6 output{};
+    for (auto const& c : testCases) {
+        messages::TestMessage6 input{
+            .value1 = c,
+        };
+        messages::TestMessage6 output{};
 
-    using WS = typename TestType::WS;
-    using RS = typename TestType::RS;
-    cppRoundTrip<WS, RS>(irSpan, input, output);
+        using WS = typename TestType::WS;
+        using RS = typename TestType::RS;
+        cppRoundTrip<WS, RS>(irSpan, input, output);
 
-    REQUIRE(input == output);
+        REQUIRE(input == output);
+    }
 }
 
 TEMPLATE_LIST_TEST_CASE("Simple message 7 round trip 1",
                         "[simple]",
                         StreamTypes) {
-    messages::TestMessage7 input{
-        .value = (uint64_t)1,
+    std::vector<uint64_t> testCases{
+        0,
+        1,
+        std ::numeric_limits<uint64_t>::max() - 2,
+        std ::numeric_limits<uint64_t>::max() - 1,
+        std ::numeric_limits<uint64_t>::max(),
     };
-    messages::TestMessage7 output{};
+    for (auto c : testCases) {
+        messages::TestMessage7 input{
+            .value = c,
+        };
+        messages::TestMessage7 output{};
 
-    using WS = typename TestType::WS;
-    using RS = typename TestType::RS;
-    cppRoundTrip<WS, RS>(irSpan, input, output);
+        using WS = typename TestType::WS;
+        using RS = typename TestType::RS;
+        cppRoundTrip<WS, RS>(irSpan, input, output);
 
-    REQUIRE(input == output);
+        REQUIRE(input == output);
+    }
 }
 TEMPLATE_LIST_TEST_CASE("Simple message 7 round trip 2",
                         "[simple]",
                         StreamTypes) {
-    messages::TestMessage7 input{
-        .value = (int64_t)1,
+    std::vector<int64_t> testCases{
+        0,
+        1,
+        -1,
+        std::numeric_limits<int64_t>::max(),
+        std::numeric_limits<int64_t>::max() - 1,
+        std::numeric_limits<int64_t>::min(),
+        std::numeric_limits<int64_t>::min() + 1,
     };
-    messages::TestMessage7 output{};
+    for (auto c : testCases) {
+        messages::TestMessage7 input{
+            .value = (int64_t)c,
+        };
+        messages::TestMessage7 output{};
 
-    using WS = typename TestType::WS;
-    using RS = typename TestType::RS;
-    cppRoundTrip<WS, RS>(irSpan, input, output);
+        using WS = typename TestType::WS;
+        using RS = typename TestType::RS;
+        cppRoundTrip<WS, RS>(irSpan, input, output);
 
-    REQUIRE(input == output);
+        REQUIRE(input == output);
+    }
 }
